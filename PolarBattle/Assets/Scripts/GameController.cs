ec98e171e7cb1bb18596cf817d070b96ac3dd002
rainbee2214 <rainbee2214@gmail.com>
2014-12-06 00:33:09 -0400

    Project baseline. Basic structure to start a game.

---
+++ PolarBattle/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+    public static GameController controller;
+
+    void Awake()
+    {
+        //if control is not set, set it to this one and allow it to persist
+        if (controller == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            controller = this;
+        }
+        //else if control exists and it isn't this instance, destroy this instance
+        else if (controller != this)
+        {
+            //Debug.Log("Game control already exists, deleting this new one");
+            Destroy(gameObject);
+        }
+    }
+
+	void Start () 
+	{
+	}
+
+	void Update () 
+	{
+	
+	}
+}
---
+++ PolarBattle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    #region Properties
+    private int score;
+    public int Score
+    {
+        get { return score; }
+        set { score += value; }
+    }
+
+    private bool isDead;
+    public bool IsDead
+    {
+        get { return isDead; }
+        set { isDead = value; }
+    }
+
+    private int level;
+    public int Level
+    {
+        get { return level; }
+        set { level += value; }
+    }
+    #endregion
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}
